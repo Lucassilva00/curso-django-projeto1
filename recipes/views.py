e07e8aad9c2566d09f8bfdf6e1068cfb37522f4c
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-03-14 17:25:43 -0300

    Removido sobre e contato

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # HTTP request <- HTTP response
@@ -9,11 +8,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Lucas'
     })
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
