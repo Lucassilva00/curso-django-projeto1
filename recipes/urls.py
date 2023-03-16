4647e62f0272d582f5e7d0867100af4c31afb78f
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-03-16 16:14:46 -0300

    add new url and view

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
+
+# Por urls ser irmão de views é possível trocar o nome do 'pai' recipes
+# por um .(ponto)
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe)
 ]
