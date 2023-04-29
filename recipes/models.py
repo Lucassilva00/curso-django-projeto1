fc08f1bb387538caaa293674602cf02e9eab7e2c
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-04-29 14:06:32 -0300

    Registro de Recipe em admin django

--- recipes/models.py
+++ recipes/models.py
@@ -13,7 +13,6 @@ class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
-    preparation_time = models.IntegerField
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     serving_unit = models.CharField(max_length=65)
@@ -28,3 +27,6 @@ class Recipe(models.Model):
         Category, on_delete=models.SET_NULL, null=True)
 
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
