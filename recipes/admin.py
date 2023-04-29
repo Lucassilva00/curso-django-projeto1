7b1d8b82c8b73565968ad9df4e95564c4ee89a1e
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-04-29 13:17:35 -0300

    Registro model Category no admin do django

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
+
 # Register your models here.
