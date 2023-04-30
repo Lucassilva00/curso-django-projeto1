beac2ffb47247c64da8298f0b70864777c2e7213
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-04-30 18:46:14 -0300

    Atalho get_list_or_404 e Detalhes da receita

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+# from django.http import Http404 - igual ao que está embaixo, if1
+# from django.http import HttpResponse - Para o if2 com comentário.
+# Serve para mostrar página que não existe.
+
+from django.shortcuts import get_list_or_404, render
 
 from utils.recipes.factory import make_recipe
 
@@ -16,15 +20,26 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+
+    # 1 if not recipes:
+    # 1    raise Http404('Not Found')
+
+    # 2 if not recipes:
+    # 2    return HttpResponse(content='Not Found', status=404)
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        id=id, is_published=True).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
