43fb3c73996f25c5cc3ba70c805836a932e551bd
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-04-30 14:21:22 -0300

    Adicionado o campo preparation_time

--- recipes/models.py
+++ recipes/models.py
@@ -13,6 +13,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
+    preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     serving_unit = models.CharField(max_length=65)
