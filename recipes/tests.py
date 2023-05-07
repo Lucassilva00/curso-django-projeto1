e76a2b40b79c335fb5b85ea4369825526318b67c
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-05-07 19:47:16 -0300

    Iniciando com testes no Django

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        print('Olá Mundo!')
+        assert 1 == 1
