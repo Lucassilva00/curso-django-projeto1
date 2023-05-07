1ea873e759c185e7612f90e11b6dad996699e068
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-05-07 20:27:45 -0300

    Assert, Django reverse url e Pytest-watch

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        print('Olá Mundo!')
-        assert 1 == 1
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
