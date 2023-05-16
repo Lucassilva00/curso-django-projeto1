b7f6c14d01fdcd224af1523fdc9a9ba10e48295c
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-05-16 20:51:23 -0300

    Testando "No recipes found" se não existir receita

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -24,3 +24,8 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_view_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn('<h2>No recipes found here!</h2>',
+                      response.content.decode('utf-8'))
