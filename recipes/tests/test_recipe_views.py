f169a120bdff26207d8007ea6f2011187be93a7e
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-07-04 10:30:19 -0300

    Testando is_published das views category e detail

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -65,6 +65,13 @@ class RecipeViewsTest(RecipeTestBase):
 
         self.assertIn(needed_title, content)
 
+    def test_recipe_category_template_dont_load_not_published_recipe(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
@@ -81,3 +88,10 @@ class RecipeViewsTest(RecipeTestBase):
         content = response.content.decode('utf-8')
 
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_not_published_recipe(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id}))
+
+        self.assertEqual(response.status_code, 404)
