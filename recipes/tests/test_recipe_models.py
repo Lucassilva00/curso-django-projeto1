ac845e88964167c900c4b848713f205f013eae7a
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-07-04 21:59:14 -0300

    Teste valores default do model

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
 
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import Recipe, RecipeTestBase
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -9,10 +9,28 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
+    def make_recipes_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='categoria teste'),
+            author=self.make_author(username='newusertest'),
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            serving_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
+
     # self.recipe.save()  # O django salva na base de dados
     # self.fail(self.recipe.title) Falha o código de propósito
 
     # parametriza um teste, o .expand é usado por ter uma lista
+
     @parameterized.expand([
         ('title', 65),
         ('description', 165),
@@ -31,3 +49,13 @@ class RecipeModelTest(RecipeTestBase):
         with self.assertRaises(ValidationError):
             # Executa validação e não trava o teste se não passar na validação
             self.recipe.full_clean()
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipes_no_defaults()
+        self.assertFalse(recipe.preparation_steps_is_html,
+                         msg='Recipe preparation_steps_is_html is not False')
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipes_no_defaults()
+        self.assertFalse(recipe.is_published,
+                         msg='Recipe is_published is not False')
