c777faf30a8c64dda16b6a63dd5571fc66ebaef2
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-07-06 07:56:08 -0300

    Criado nova URL para search com método TDD

--- recipes/urls.py
+++ recipes/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('recipes/category/<int:category_id>/',
          views.category, name="category"),
-    path('recipes/<int:id>/', views.recipe, name="recipe")
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/search/', lambda request: ..., name="search"),
 ]
