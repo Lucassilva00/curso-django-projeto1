e75f60a97ded09935775d8dda30fb0b7aa0b0f33
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-07-11 10:35:19 -0300

    Criando a função make_pagination

--- recipes/views.py
+++ recipes/views.py
@@ -7,8 +7,7 @@ from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from utils.pagination import make_pagination_range
-from utils.recipes.factory import make_recipe
+from utils.pagination import make_pagination
 
 from .models import Recipe
 
@@ -19,27 +18,23 @@ from .models import Recipe
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    try:
-        current_page = int(request.GET.get('page', 1))
-    except:
-        current_page = 1
-    paginator = Paginator(recipes, 9)
-    page_obj = paginator.get_page(current_page)
-
-    pagination_range = make_pagination_range(
-        paginator.page_range,
-        4,
-        current_page
+    page_obj, pagination_range = make_pagination(
+        request, recipes, 9
     )
 
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes, 'recipes': page_obj, 'pagination_range': pagination_range
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
     })
 
 
 def category(request, category_id):
     recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id'))
+
+    page_obj, pagination_range = make_pagination(
+        request, recipes, 9
+    )
 
     # 1 if not recipes:
     # 1    raise Http404('Not Found')
@@ -48,7 +43,7 @@ def category(request, category_id):
     # 2    return HttpResponse(content='Not Found', status=404)
 
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj, 'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name} - Category | '
     })
 
@@ -75,8 +70,13 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(
+        request, recipes, 9
+    )
+
     return render(request, 'recipes/pages/search.html',
                   {'page_title': f'Search for "{search_term}" |',
                    'search_term': search_term,
-                   'recipes': recipes,
+                   'recipes': page_obj, 'pagination_range': pagination_range,
+                   'additional_url_query': f'&q={search_term}',
                    })
