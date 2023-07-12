f9f09b49dd32514ffa0bf9f7dde7944290cf0460
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-07-12 10:32:17 -0300

    Removendo mensagens desnecessárias

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,6 @@
 
 import os
 
-from django.contrib import messages
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -26,10 +25,6 @@ def home(request):
     page_obj, pagination_range = make_pagination(
         request, recipes, PER_PAGE
     )
-
-    messages.error(request, 'Que legal sua mensagem foi um sucesso!')
-    messages.success(request, 'Epa, você foi pesquisar algo que eu vi.')
-    messages.info(request, 'Epa, você foi pesquisar algo que eu vi.')
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
