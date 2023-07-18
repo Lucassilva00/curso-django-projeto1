7d761cfd8035c6eec0b4c9f2202840961d2d46ed
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-07-18 20:13:30 -0300

    labels, widgets, erros, atributos dos campos

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,36 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        # semelhante a criação pelo método acima de escolher os campos, com exclude todos campos são selecionados menos aqueles presentes no exclude # noqa E501
+        # exclude = ['first_name']
+
+        labels = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'Email',
+            'password': 'Password',
+        }
+
+        # Altera os help texts
+        help_texts = {
+            'email': 'The email must be valid'
+        }
+
+        # Altera as mensagens de erro
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty'
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your name here...',
+                # 'class': 'input text-input' #É possível add classe também
+            }),
+
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here...'
+            }),
+        }
