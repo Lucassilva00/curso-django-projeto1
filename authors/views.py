3a4332c222dad5ae10d88a273799e9afa8e50d25
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-07-18 11:04:56 -0300

    Criando campos do formulário de user manualmente

--- authors/views.py
+++ authors/views.py
@@ -6,7 +6,12 @@ from .forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm()
+
+    if request.POST:
+        form = RegisterForm(request.POST)
+
+    else:
+        form = RegisterForm()
 
     return render(request, 'authors/pages/register_view.html', {
         'form': form
