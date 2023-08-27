b5d17712dffccd1937f51c5ec3f2d4fe8634f34a
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-08-27 19:08:04 -0300

    Integration test

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from django.test import TestCase as DjangoTestCase
+from django.urls import reverse
 from parameterized import parameterized
 
 from authors.forms import RegisterForm
@@ -46,3 +48,25 @@ class AuthorRegisterFormUnitTest(TestCase):
         label = form[field].field.label
 
         self.assertEqual(label, needed)
+
+
+class AuthorRegisterFormIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@email.com',
+            'password': 'str0ngP@ssword1',
+            'password2': 'str0ngP@ssword1',
+        }
+        return super().setUp(*args, **kwargs)
+
+    @parameterized.expand([
+        ('username', 'This field must not be empty'),
+    ])
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
