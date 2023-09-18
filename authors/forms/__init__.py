1c2362a0879cc5fb854e05f37098a81492ca9f69
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-09-18 18:34:47 -0300

    Criando formulário para login

---
+++ authors/forms/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa
+
+from .login import LoginForm
+from .register_form import RegisterForm
---
+++ utils/django_forms.py
@@ -0,0 +1,23 @@
+import re
+
+from django.core.exceptions import ValidationError
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+
+def strong_password(password):
+    # regex vem de regular expression
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError('Password must have at least one uppercase letter, '  # noqa E501
+                              'one lowercase letter and one number. The length should be '  # noqa E501
+                              'at least 8 characters.',
+                              code='invalid',)
